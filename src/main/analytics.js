8f15773ae78fa0ffad6c1edcbd807d607682e51b
Leo Lamprecht <mindrun@icloud.com>
2016-10-11 13:53:51 +0200

    Bind analytics to user

--- src/main/analytics.js
+++ src/main/analytics.js
@@ -9,6 +9,7 @@ import md5 from 'md5'
 import mixpanel from 'mixpanel'
 import fileSize from 'filesize'
 import firstRun from 'first-run'
+import Config from 'electron-config'
 
 // Ours
 import pkg from '../../app/package'
@@ -24,6 +25,17 @@ const getMacAddress = () => new Promise((resolve, reject) => {
     resolve(mac)
   })
 })
+
+const getEmailAddress = () => {
+  const config = new Config()
+  const identifier = 'now.user.email'
+
+  if (!config.has(identifier)) {
+    return false
+  }
+
+  return config.get(identifier)
+}
 
 const analytics = mixpanel.init(pkg.mixPanel.id, {
   protocol: 'https'
@@ -44,16 +56,23 @@ export const track = (handle, details = {}) => {
 export const init = async () => {
   process.env.MACHINE = md5(await getMacAddress())
 
-  analytics.people.set(process.env.MACHINE, {
-    $email: 'lol@lol.de'
-  })
+  const person = {
+    Platform: os.type(),
+    Memory: fileSize(os.totalmem()),
+    Arch: os.arch()
+  }
+
+  const email = getEmailAddress()
+
+  if (email) {
+    person.$email = email
+  }
+
+  analytics.people.set(process.env.MACHINE, person)
 
   if (firstRun()) {
     track('App installed', {
-      Version: pkg.version,
-      Platform: os.type(),
-      Memory: fileSize(os.totalmem()),
-      Arch: os.arch()
+      Version: pkg.version
     })
   }
 
