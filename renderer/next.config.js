0e63a9fb5cd818adb9c543e622c6c96d90a524e8
Leo Lamprecht <mindrun@icloud.com>
2017-03-29 19:16:14 +0200

    Don't minify code to allow for better prod debugging

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   webpack(cfg) {
     cfg.target = 'electron-renderer';
+
+    cfg.plugins = cfg.plugins.filter(plugin => {
+      if (plugin.constructor.name === 'UglifyJsPlugin') {
+        return false;
+      }
+
+      return true;
+    });
+
     return cfg;
   }
 };
