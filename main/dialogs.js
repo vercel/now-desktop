b5d268a6c2e839c74e3fee52573dece37c2dce3d
Leo Lamprecht <mindrun@icloud.com>
2017-04-01 20:21:49 +0200

    Log trace when showing errors

--- main/dialogs.js
+++ main/dialogs.js
@@ -62,6 +62,11 @@ exports.deploy = async function(tray) {
 };
 
 exports.error = function(detail, trace, win) {
+  // We need to log the error in order to be able to inspect it
+  if (trace) {
+    console.error(trace);
+  }
+
   dialog.showMessageBox(win || null, {
     type: 'error',
     message: 'An Error Occurred',
