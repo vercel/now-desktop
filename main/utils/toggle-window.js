f63988009c3e17c50e699bded1910fc8829451d9
Leo Lamprecht <mindrun@icloud.com>
2017-04-24 14:53:45 +0200

    Make sure window isn't outside screen

--- main/utils/toggle-window.js
+++ main/utils/toggle-window.js
@@ -1,4 +1,36 @@
+const positionWindow = (tray, window) => {
+  // This module needs to be loaded after the app is ready
+  // I don't know why, but that's required by electron
+  const { screen } = require('electron')
+
+  const trayBounds = tray.getBounds()
+  const windowWidth = window.getSize()[0]
+  const trayCenter = trayBounds.x + trayBounds.width / 2
+
+  let horizontalPosition = trayCenter - windowWidth / 2
+  const verticalPosition = trayBounds.height + 6
+
+  // Find the current display
+  const display = screen.getDisplayMatching(trayBounds)
+
+  if (display) {
+    const displayWidth = display.workAreaSize.width
+
+    const left = horizontalPosition + windowWidth
+    const maxLeft = displayWidth - 20
+
+    // Check if window would be outside screen
+    // If yes, make sure it isn't
+    if (left > maxLeft) {
+      horizontalPosition -= left - maxLeft
+    }
+  }
+
+  window.setPosition(horizontalPosition, verticalPosition)
+}
+
 module.exports = (event, window, tray) => {
+  // Check if the current window is visible
   const visible = window.isVisible()
 
   if (event) {
@@ -19,15 +51,9 @@ module.exports = (event, window, tray) => {
   if (visible) {
     window.close()
   } else {
+    // Position main window correctly under the tray icon
     if (global.windows && window === global.windows.main) {
-      const trayBounds = tray.getBounds()
-      const windowWidth = window.getSize()[0]
-      const trayCenter = trayBounds.x + trayBounds.width / 2
-
-      const horizontalPosition = trayCenter - windowWidth / 2
-      const verticalPosition = trayBounds.height + 6
-
-      window.setPosition(horizontalPosition, verticalPosition)
+      positionWindow(tray, window)
     }
 
     window.show()
