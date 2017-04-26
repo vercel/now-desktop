cb0986f06996ac44cb1882dcd5e6a96c4aaf5fcf
Leo Lamprecht <mindrun@icloud.com>
2017-04-27 01:11:57 +0200

    Proper login message

--- renderer/components/feed/event.js
+++ renderer/components/feed/event.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 
 // Utilities
 import remote from '../../utils/electron'
+import parseUA from '../../utils/user-agent'
 
 class EventMessage extends React.Component {
   openURL(event) {
@@ -12,6 +13,46 @@ class EventMessage extends React.Component {
 
     const url = event.target.innerHTML
     remote.shell.openExternal(`https://${url}`)
+  }
+
+  loginMessage(payload) {
+    let { userAgent, geolocation } = payload
+    userAgent = parseUA(userAgent)
+
+    const osNames = {
+      darwin: 'Mac OS',
+      win32: 'Windows',
+      linux: 'Linux',
+      freebsd: 'FreeBSD',
+      sunos: 'SunOS'
+    }
+
+    let from
+    let os
+
+    if (userAgent) {
+      from = userAgent.browser
+        ? userAgent.browser.name
+        : userAgent.program ? 'CLI' : null
+      os = osNames[userAgent.os.name] || userAgent.os.name
+    } else {
+      from = payload.env
+      os = payload.os
+    }
+
+    let message = 'logged in'
+    if (from) message += ` from ${from}`
+    if (os) message += ` (${os})`
+
+    if (geolocation) {
+      if (geolocation.city === geolocation.regionName) {
+        message += ` in ${geolocation.city}`
+      } else {
+        message += ` in ${geolocation.city}, ${geolocation.regionName}`
+      }
+    }
+
+    return message
   }
 
   getDescription() {
@@ -25,6 +66,14 @@ class EventMessage extends React.Component {
         'deployed ',
         <a className="link" onClick={this.openURL}>{details.payload.url}</a>
       ])
+    }
+
+    if (type === 'deployment-delete') {
+      list.push(['deleted ', <b>{details.payload.url}</b>])
+    }
+
+    if (type === 'login') {
+      list.push(this.loginMessage(details.payload))
     }
 
     return list
