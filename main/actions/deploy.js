1b59f0965da716e70686e0e84a0e37625ed39d73
Guillermo Rauch <rauchg@gmail.com>
2017-05-15 07:14:34 -0700

    Refactored upload to be much faster (#303)
    * refactor uploads
    
    * fix share menu item

--- main/actions/deploy.js
+++ main/actions/deploy.js
@@ -1,22 +1,43 @@
 // Native
 const path = require('path')
+const { basename } = require('path')
+const { homedir } = require('os')
+const qs = require('querystring')
 
 // Packages
 const { clipboard, shell, dialog } = require('electron')
-const fs = require('fs-promise')
+const { EventEmitter } = require('events')
+const { parse: parseUrl } = require('url')
 const pathExists = require('path-exists')
-const deglob = require('deglob')
-const { isTextSync: isText } = require('istextorbinary')
-const chalk = require('chalk')
-const slash = require('slash')
+const splitArray = require('split-array')
+const resumer = require('resumer')
+const retry = require('async-retry')
+const { parse: parseIni } = require('ini')
+const { readFile, stat, lstat } = require('fs-promise')
+const bytes = require('bytes')
 
 // Ours
-const { connector } = require('../api')
+const Agent = require('../utils/agent')
 const { error: showError } = require('../dialogs')
 const notify = require('../notify')
+const { get: getConfig } = require('../utils/config')
+const {
+  staticFiles: getFiles,
+  npm: getNpmFiles,
+  docker: getDockerFiles
+} = require('../utils/get-files')
+const hash = require('../utils/hash')
+const ua = require('../utils/ua')
+const readMetaData = require('../utils/read-metadata')
+
+// How many concurrent HTTP/2 stream uploads
+const MAX_CONCURRENT = 4
+
+const IS_WIN = process.platform.startsWith('win')
+const SEP = IS_WIN ? '\\' : '/'
 
 const getProjectType = (nodeReady, dockerReady) => {
-  let projectType = 'docker'
+  let projectType = 'static'
 
   if (nodeReady && dockerReady) {
     const dialogAnswer = dialog.showMessageBox({
@@ -27,253 +48,507 @@ const getProjectType = (nodeReady, dockerReady) => {
     })
 
     if (!dialogAnswer) {
-      projectType = 'node'
+      projectType = 'npm'
     }
   } else if (nodeReady) {
-    projectType = 'node'
+    projectType = 'npm'
   }
 
   return projectType
 }
 
-const genTitle = (deployment, sharing) => {
-  if (deployment.state === 'READY') {
-    return 'Already deployed!'
+class Now extends EventEmitter {
+  constructor({
+    apiUrl = 'https://api.zeit.co',
+    token,
+    currentTeam,
+    forceNew = false,
+    debug = false
+  }) {
+    super()
+    this._token = token
+    this._debug = debug
+    this._forceNew = forceNew
+    this._agent = new Agent(apiUrl, { debug })
+    this._onRetry = this._onRetry.bind(this)
+    this.currentTeam = currentTeam
   }
 
-  return (sharing ? 'Sharing' : 'Deploying') + '...'
-}
+  async create(
+    path,
+    {
+      wantsPublic,
+      quiet = false,
+      env = {},
+      followSymlinks = true,
+      forceNew = false,
+      forceSync = false,
+      forwardNpm = false,
 
-const getContents = async dir => {
-  let items
+      // From readMetaData
+      name,
+      description,
+      type = 'npm',
+      pkg = {},
+      nowConfig = {},
+      hasNowJson = false
+    }
+  ) {
+    this._path = path
+    this._isFile = await isFile(path)
 
-  try {
-    items = await new Promise((resolve, reject) => {
-      deglob(
-        ['**'],
-        {
-          cwd: dir
-        },
-        (err, files) => {
-          if (err) {
-            reject(err)
-            return
-          }
+    let files
+    let engines
 
-          resolve(files)
-        }
+    if (this._debug) {
+      console.time('> [debug] Getting files')
+    }
+
+    const opts = { debug: this._debug, hasNowJson }
+    if (type === 'npm') {
+      files = await getNpmFiles(path, pkg, nowConfig, opts)
+
+      // A `start` or `now-start` npm script, or a `server.js` file
+      // in the root directory of the deployment are required
+      if (!hasNpmStart(pkg) && !hasFile(path, files, 'server.js')) {
+        const err = new Error(
+          'Missing `start` (or `now-start`) script in `package.json`. ' +
+            'See: https://docs.npmjs.com/cli/start.'
+        )
+        err.userError = true
+        throw err
+      }
+
+      engines = nowConfig.engines || pkg.engines
+      forwardNpm = forwardNpm || nowConfig.forwardNpm
+    } else if (type === 'static') {
+      files = await getFiles(path, nowConfig, opts)
+    } else if (type === 'docker') {
+      files = await getDockerFiles(path, nowConfig, opts)
+    }
+
+    if (this._debug) {
+      console.timeEnd('> [debug] Getting files')
+    }
+
+    // Read `registry.npmjs.org` authToken from .npmrc
+    let authToken
+    if (type === 'npm' && forwardNpm) {
+      authToken =
+        (await readAuthToken(path)) || (await readAuthToken(homedir()))
+    }
+
+    if (this._debug) {
+      console.time('> [debug] Computing hashes')
+    }
+
+    const pkgDetails = Object.assign({ name }, pkg)
+    const hashes = await hash(files, pkgDetails)
+
+    if (this._debug) {
+      console.timeEnd('> [debug] Computing hashes')
+    }
+
+    this._files = hashes
+
+    const deployment = await this.retry(async bail => {
+      if (this._debug) {
+        console.time('> [debug] /now/create')
+      }
+
+      // Flatten the array to contain files to sync where each nested input
+      // array has a group of files with the same sha but different path
+      const files = await Promise.all(
+        Array.prototype.concat.apply(
+          [],
+          await Promise.all(
+            Array.from(this._files).map(async ([sha, { data, names }]) => {
+              const statFn = followSymlinks ? stat : lstat
+
+              return names.map(async name => {
+                const getMode = async () => {
+                  const st = await statFn(name)
+                  return st.mode
+                }
+
+                const mode = await getMode()
+
+                return {
+                  sha,
+                  size: data.length,
+                  file: this._isFile
+                    ? basename(this._path)
+                    : toRelative(name, this._path),
+                  mode
+                }
+              })
+            })
+          )
+        )
       )
+
+      const res = await this._fetch('/now/create', {
+        method: 'POST',
+        body: {
+          env,
+          public: wantsPublic || nowConfig.public,
+          forceNew,
+          forceSync,
+          name,
+          description,
+          deploymentType: type,
+          registryAuthToken: authToken,
+          files,
+          engines
+        }
+      })
+
+      if (this._debug) {
+        console.timeEnd('> [debug] /now/create')
+      }
+
+      // No retry on 4xx
+      let body
+      try {
+        body = await res.json()
+      } catch (err) {
+        throw new Error('Unexpected response')
+      }
+
+      if (res.status === 429) {
+        const msg = 'You are being rate limited. Please try again later'
+        const err = new Error(msg)
+        err.status = res.status
+        err.retryAfter = 'never'
+        return bail(err)
+      } else if (res.status >= 400 && res.status < 500) {
+        const err = new Error(body.error.message)
+        err.userError = true
+        return bail(err)
+      } else if (res.status !== 200) {
+        throw new Error(body.error.message)
+      }
+
+      return body
     })
-  } catch (err) {
-    showError('Could not read directory to deploy', err)
-    return
+
+    // We report about files whose sizes are too big
+    let missingVersion = false
+    if (deployment.warnings) {
+      let sizeExceeded = 0
+      deployment.warnings.forEach(warning => {
+        if (warning.reason === 'size_limit_exceeded') {
+          const { sha, limit } = warning
+          const n = hashes.get(sha).names.pop()
+          showError(`Skipping file ${n} (size exceeded ${bytes(limit)})`)
+          hashes.get(sha).names.unshift(n) // Move name (hack, if duplicate matches we report them in order)
+          sizeExceeded++
+        } else if (warning.reason === 'node_version_not_found') {
+          const { wanted, used } = warning
+          showError(
+            `Requested node version ${wanted} is not available. Used ${used}`
+          )
+          missingVersion = true
+        }
+      })
+
+      if (sizeExceeded) {
+        showError(
+          `${sizeExceeded} of the files exceeded the limit for your plan. Please upgrade`
+        )
+      }
+    }
+
+    if (!quiet && type === 'npm' && deployment.nodeVersion) {
+      if (engines && engines.node) {
+        if (missingVersion) {
+          console.log(`> Using Node.js ${deployment.nodeVersion} (default)`)
+        } else {
+          console.log(
+            `> Using Node.js ${deployment.nodeVersion} (requested: ${`\`${engines.node}\``})`
+          )
+        }
+      } else {
+        console.log(`> Using Node.js ${deployment.nodeVersion} (default)`)
+      }
+    }
+
+    this._id = deployment.deploymentId
+    this._name = pkgDetails.name || name
+    this._host = deployment.url
+    this._missing = deployment.missing || []
+
+    return this._url
   }
 
-  return items
-}
-
-const removeTempDir = async (dir, logStatus) => {
-  try {
-    await fs.remove(dir)
-  } catch (err) {
-    showError('Could not remove temporary directory', err)
-    return
+  get id() {
+    return this._id
   }
 
-  logStatus('Removed temporary directory')
+  get url() {
+    return `https://${this._host}`
+  }
+
+  get host() {
+    return this._host
+  }
+
+  get syncAmount() {
+    if (!this._syncAmount) {
+      this._syncAmount = this._missing
+        .map(sha => this._files.get(sha).data.length)
+        .reduce((a, b) => a + b, 0)
+    }
+    return this._syncAmount
+  }
+
+  _fetch(_url, opts = {}) {
+    if (!opts.useCurrentTeam && this.currentTeam) {
+      const parsedUrl = parseUrl(_url, true)
+      const query = parsedUrl.query
+      query.teamId = this.currentTeam.id
+      _url = `${parsedUrl.pathname}?${qs.encode(query)}`
+      delete opts.useCurrentTeam
+    }
+
+    opts.headers = opts.headers || {}
+    opts.headers.authorization = `Bearer ${this._token}`
+    opts.headers['user-agent'] = ua
+    return this._agent.fetch(_url, opts)
+  }
+
+  upload() {
+    const parts = splitArray(this._missing, MAX_CONCURRENT)
+
+    if (this._debug) {
+      console.log(
+        '> [debug] Will upload ' +
+          `${this._missing.length} files in ${parts.length} ` +
+          `steps of ${MAX_CONCURRENT} uploads.`
+      )
+    }
+
+    const uploadChunk = () => {
+      Promise.all(
+        parts.shift().map(sha =>
+          retry(
+            async (bail, attempt) => {
+              const file = this._files.get(sha)
+              const { data, names } = file
+
+              if (this._debug) {
+                console.time(`> [debug] /sync #${attempt} ${names.join(' ')}`)
+              }
+
+              const stream = resumer().queue(data).end()
+              const res = await this._fetch('/now/sync', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/octet-stream',
+                  'Content-Length': data.length,
+                  'x-now-deployment-id': this._id,
+                  'x-now-sha': sha,
+                  'x-now-file': names
+                    .map(name => {
+                      return this._isFile
+                        ? basename(this._path)
+                        : toRelative(encodeURIComponent(name), this._path)
+                    })
+                    .join(','),
+                  'x-now-size': data.length
+                },
+                body: stream
+              })
+
+              if (this._debug) {
+                console.timeEnd(
+                  `> [debug] /sync #${attempt} ${names.join(' ')}`
+                )
+              }
+
+              // No retry on 4xx
+              if (
+                res.status !== 200 && (res.status >= 400 || res.status < 500)
+              ) {
+                if (this._debug) {
+                  console.log(
+                    '> [debug] bailing on creating due to %s',
+                    res.status
+                  )
+                }
+
+                return bail(await responseError(res))
+              }
+
+              this.emit('upload', file)
+            },
+            { retries: 3, randomize: true, onRetry: this._onRetry }
+          )
+        )
+      )
+        .then(() => (parts.length ? uploadChunk() : this.emit('complete')))
+        .catch(err => this.emit('error', err))
+    }
+
+    uploadChunk()
+  }
+
+  retry(fn, { retries = 3, maxTimeout = Infinity } = {}) {
+    return retry(fn, {
+      retries,
+      maxTimeout,
+      onRetry: this._onRetry
+    })
+  }
+
+  _onRetry(err) {
+    if (this._debug) {
+      console.log(`> [debug] Retrying: ${err}\n${err.stack}`)
+    }
+  }
+
+  close() {
+    this._agent.close()
+  }
 }
 
 module.exports = async (folder, sharing) => {
-  const details = {}
   const dir = path.resolve(folder)
 
   process.env.BUSYNESS = 'deploying'
 
   const pkgFile = path.join(dir, 'package.json')
   const dockerFile = path.join(dir, 'Dockerfile')
-
   const dockerReady = await pathExists(dockerFile)
   const nodeReady = await pathExists(pkgFile)
+  const type = getProjectType(nodeReady, dockerReady)
 
-  // Ignore the project if there's no package file
-  if (!nodeReady && !dockerReady) {
-    showError('Not a valid project!')
+  let config
+  try {
+    config = await getConfig()
+  } catch (err) {
+    showError('Error reading configuration')
     return
   }
 
-  notify({
-    title: 'Uploading Files...',
-    body: 'Your files are being uploaded to now.'
+  const now = new Now({ type, token: config.token, debug: true })
+
+  now.on('error', err => {
+    showError(err.message)
   })
 
-  // Log separator
-  if (!sharing) {
-    console.log(chalk.grey('---'))
-  }
-
-  let projectName = 'docker project'
-
-  const projectType = getProjectType(nodeReady, dockerReady)
-  const propertyName = projectType === 'node' ? 'package' : 'package.json'
-
-  if (nodeReady) {
-    // Load the package file
-    let packageJSON
-
-    try {
-      packageJSON = await fs.readJSON(pkgFile)
-    } catch (err) {
-      showError('Not able to load package file', err)
-      return
-    }
-
-    details[propertyName] = projectType === 'docker'
-      ? JSON.stringify(packageJSON)
-      : packageJSON
-  }
-
-  if (projectType === 'node') {
-    projectName = details[propertyName].name
-  }
-
-  const logStatus = message =>
-    console.log(chalk.yellow(`[${projectName}]`) + ' ' + message)
-
-  const items = await getContents(dir)
-  let existing = []
-
-  for (const itemPath of items) {
-    existing.push(pathExists(itemPath))
-  }
-
   try {
-    existing = await Promise.all(existing)
+    await now.create(
+      dir,
+      await readMetaData(dir, {
+        deploymentType: type
+      })
+    )
   } catch (err) {
-    showError('Not able to check if path exists', err)
+    showError(err.message)
     return
   }
 
-  let readers = []
+  const { url } = now
 
-  for (let i = 0; i < existing.length; i++) {
-    const exists = existing[i]
-    const item = items[i]
-
-    if (!exists) {
-      continue
-    }
-
-    const relativePath = slash(path.relative(dir, item))
-
-    if (relativePath === 'package.json') {
-      continue
-    }
-
-    const reader = fs.readFile(item)
-    readers.push(reader)
-  }
-
-  try {
-    readers = await Promise.all(readers)
-  } catch (err) {
-    showError('Could not read file for deployment', err)
-    return
-  }
-
-  for (let i = 0; i < readers.length; i++) {
-    const file = readers[i]
-    const filePath = items[i]
-
-    const itemDetails = path.parse(filePath)
-    const fileName = itemDetails.base
-    const relativePath = slash(path.relative(dir, filePath))
-
-    // Find out if the file is text-based or binary
-    const fileIsText = isText(fileName, file)
-
-    if (!fileIsText) {
-      details[relativePath] = {
-        binary: true,
-        content: file.toString('base64')
-      }
-
-      continue
-    }
-
-    details[relativePath] = file.toString()
-  }
-
-  let deployment
-  const apiSession = await connector()
-
-  try {
-    deployment = await apiSession.createDeployment(details)
-  } catch (err) {
-    showError('Could not create deployment', err.toString())
-    return
-  }
-
-  const url = `https://${deployment.host}`
-
-  if (deployment.state === 'READY') {
-    // Open the URL in the default browser
-    shell.openExternal(url)
-
-    // Log the current state of the deployment
-    logStatus(deployment.state)
-  } else {
-    // If the deployment isn't ready, regularly check for the state
-    const checker = setInterval(async () => {
-      let current
-
-      try {
-        current = await apiSession.getDeployment(deployment.uid)
-      } catch (err) {
-        if (err.includes('404')) {
-          clearInterval(checker)
-          return
-        }
-
-        showError('Not able to get deployment', err)
-        return
-      }
-
-      if (current.state === 'READY') {
-        clearInterval(checker)
-
-        process.env.BUSYNESS = 'ready'
-
-        notify({
-          title: 'Done ' + (sharing ? 'Sharing' : 'Deploying') + '!',
-          body: 'Opening the URL in your browser...',
-          url
-        })
-
-        // Open the URL in the default browser
-        shell.openExternal(url)
-      }
-
-      if (current.state === 'DELETED') {
-        clearInterval(checker)
-      }
-
-      // Log the current state of the deployment
-      logStatus(current.state)
-    }, 3000)
-  }
+  // Open the URL in the default browser
+  shell.openExternal(url)
 
   // Copy deployment URL to clipboard
   clipboard.writeText(url)
 
   // Let the user now
   notify({
-    title: genTitle(deployment, sharing),
-    body: 'Your clipboard already contains the URL.',
+    title: (sharing ? 'Sharing' : 'Deploying') + '...',
+    body: 'Your clipboard contains the URL.',
     url
   })
 
-  // Delete the local deployed directory if required
-  if (sharing) {
-    await removeTempDir(folder, logStatus)
+  function ready() {
+    now.close()
   }
+
+  if (now.syncAmount) {
+    now.upload()
+
+    now.on('upload', ({ names, data }) => {
+      console.log(
+        `> [debug] Uploaded: ${names.join(' ')} (${bytes(data.length)})`
+      )
+    })
+
+    now.on('complete', ready)
+  } else {
+    ready()
+  }
+}
+
+function hasNpmStart(pkg) {
+  return pkg.scripts && (pkg.scripts.start || pkg.scripts['now-start'])
+}
+
+function hasFile(base, files, name) {
+  const relative = files.map(file => toRelative(file, base))
+  return relative.indexOf(name) !== -1
+}
+
+function toRelative(path, base) {
+  const fullBase = base.endsWith(SEP) ? base : base + SEP
+  let relative = path.substr(fullBase.length)
+
+  if (relative.startsWith(SEP)) {
+    relative = relative.substr(1)
+  }
+
+  return relative.replace(/\\/g, '/')
+}
+
+async function responseError(res) {
+  let message
+  let userError
+
+  if (res.status >= 400 && res.status < 500) {
+    let body
+
+    try {
+      body = await res.json()
+    } catch (err) {
+      body = {}
+    }
+
+    // Some APIs wrongly return `err` instead of `error`
+    message = (body.error || body.err || {}).message
+    userError = true
+  } else {
+    userError = false
+  }
+
+  const err = new Error(message || 'Response error')
+  err.status = res.status
+  err.userError = userError
+
+  if (res.status === 429) {
+    const retryAfter = res.headers.get('Retry-After')
+
+    if (retryAfter) {
+      err.retryAfter = parseInt(retryAfter, 10)
+    }
+  }
+
+  return err
+}
+
+async function readAuthToken(path, name = '.npmrc') {
+  try {
+    const contents = await readFile(path.resolve(path, name), 'utf8')
+    const npmrc = parseIni(contents)
+    return npmrc['//registry.npmjs.org/:_authToken']
+  } catch (err) {
+    // Do nothing
+  }
+}
+
+async function isFile(file) {
+  const s = await lstat(file)
+  return s.isFile()
 }
