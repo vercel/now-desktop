ee4173a4da07ec3cf322ecba85a58c39b4223310
Leo Lamprecht <mindrun@icloud.com>
2017-05-17 16:38:25 +0200

    Show loading indicator even if not loading bundled bin

--- renderer/components/tutorial/binary.js
+++ renderer/components/tutorial/binary.js
@@ -138,10 +138,19 @@ class Binary extends Component {
     }
 
     if (this.state.installing) {
+      const dots = this.state.progress === false ? '' : '...'
+
       return (
         <article>
-          <p>
-            <strong>{'Installing the command line interface...'}</strong>
+          <p className="install-status">
+            <strong>{'Installing the command line interface' + dots}</strong>
+
+            {this.state.progress === false &&
+              <span>
+                <i>.</i>
+                <i>.</i>
+                <i>.</i>
+              </span>}
           </p>
 
           <p>
@@ -162,6 +171,38 @@ class Binary extends Component {
               font-size: 14px;
               text-align: center;
               line-height: 22px;
+            }
+
+            .install-status i {
+              font-weight: 700;
+              font-style: normal;
+              animation-name: blink;
+              animation-duration: 1.4s;
+              animation-iteration-count: infinite;
+              animation-fill-mode: both;
+              font-size: 150%;
+            }
+
+            .install-status i:nth-child(3) {
+              animation-delay: .2s;
+            }
+
+            .install-status i:nth-child(4) {
+              animation-delay: .4s;
+            }
+
+            @keyframes blink {
+              0% {
+                opacity: 0.1;
+              }
+
+              20% {
+                opacity: 1;
+              }
+
+              100% {
+                opacity: .2;
+              }
             }
 
             .progress {
