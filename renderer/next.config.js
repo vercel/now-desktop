83f783ec9454acc3ef51e5b26adfaa239f37d4f7
Leo Lamprecht <mindrun@icloud.com>
2017-05-17 16:57:49 +0200

    Hide the critical dependency warnings

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -4,6 +4,10 @@ module.exports = {
   webpack(config, { dev }) {
     config.target = 'electron-renderer'
 
+    // Hide the "dependency is a critical expression" warnings
+    // There's no need to care about them
+    config.module.exprContextCritical = false
+
     config.plugins = config.plugins.filter(plugin => {
       return plugin.constructor.name !== 'UglifyJsPlugin'
     })
