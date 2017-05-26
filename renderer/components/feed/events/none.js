8a18a281d318152999778a362e364ccfaa41ed37
Leo Lamprecht <mindrun@icloud.com>
2017-05-26 17:21:03 +0200

    Better view for no events being available

--- renderer/components/feed/events/none.js
+++ renderer/components/feed/events/none.js
@@ -1,15 +1,28 @@
 // Packages
+import electron from 'electron'
 import React from 'react'
 import { bool } from 'prop-types'
+
+const openDocs = event => {
+  event.preventDefault()
+  const remote = electron.remote || false
+
+  if (remote) {
+    remote.shell.openExternal('https://zeit.co')
+  }
+}
 
 const NoEvents = ({ filtered }) => (
   <div>
     {filtered
       ? <h1>No Events Found!</h1>
       : [
-          <h1 key="heading">Nothing to See Here!</h1>,
+          <h1 key="heading">No Activity to Show!</h1>,
           <p key="description">
-            Drag a project into this window (or select it using the button on the top right) to trigger your first deployment.
+            Drag a project into this window (or select it using the button on the top right) to trigger
+            {' '}
+            <b onClick={openDocs}>your first deployment</b>
+            .
           </p>
         ]}
 
@@ -26,11 +39,19 @@ const NoEvents = ({ filtered }) => (
         flex-direction: column;
       }
 
+      h1 {
+        font-size: 16px;
+      }
+
       p {
         text-align: center;
-        font-size: 14px;
-        width: 290px;
-        line-height: 22px;
+        font-size: 12px;
+        width: 250px;
+        line-height: 20px;
+      }
+
+      b {
+        font-weight: 600;
       }
     `}
     </style>
