dbe30909d891e24ee6005ec1759fce64bc3fb8ce
Leo Lamprecht <mindrun@icloud.com>
2017-05-26 23:21:30 +0200

    Decide the deployment type using a package

--- main/actions/deploy.js
+++ main/actions/deploy.js
@@ -1,45 +1,24 @@
-// Native
-const path = require('path')
-
 // Packages
+const determineType = require('deployment-type')
 const pathExists = require('path-exists')
-const pathType = require('path-type')
 
 // Utilities
 const upload = require('../utils/deployment/upload')
+const { error: showError } = require('../dialogs')
 
-const determineType = async item => {
-  // If it's a file, it can only be of static nature
-  if (await pathType.file(item)) {
-    return 'static'
+module.exports = async directory => {
+  if (!await pathExists(directory)) {
+    throw new Error("Path doesn't exist!")
   }
 
-  const dockerfilePath = path.join(item, 'Dockerfile')
+  let deploymentType
 
-  // If a docker file exists, it can only be a docker deployment
-  // A pure node deployment never needs a Dockerfile
-  if (await pathExists(dockerfilePath)) {
-    return 'docker'
+  try {
+    deploymentType = await determineType(directory)
+  } catch (err) {
+    showError(err)
+    return
   }
 
-  const packagePath = path.join(item, 'package.json')
-
-  // If it's not a docker deployment and it contains
-  // a package.json file, it can only be a node deployment
-  if (await pathExists(packagePath)) {
-    return 'npm'
-  }
-
-  // If it's not a file and doesn't contain any sort of
-  // metafile, it's always of static nature
-  return 'static'
-}
-
-module.exports = async item => {
-  if (!await pathExists(item)) {
-    throw new Error(`Path doesn't exist!`)
-  }
-
-  const deploymentType = await determineType(item)
-  await upload(item, deploymentType)
+  await upload(directory, deploymentType)
 }
