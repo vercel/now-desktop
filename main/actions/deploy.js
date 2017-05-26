39908abce8873898051549367fb53b3132c23801
Leo Lamprecht <mindrun@icloud.com>
2017-05-26 22:01:21 +0200

    Pass correct deployment type

--- main/actions/deploy.js
+++ main/actions/deploy.js
@@ -41,5 +41,5 @@ module.exports = async item => {
   }
 
   const deploymentType = await determineType(item)
-  await upload(item, deploymentType === 'static')
+  await upload(item, deploymentType)
 }
