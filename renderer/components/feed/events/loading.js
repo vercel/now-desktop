ff0a3d08bddc9a4f43bca908c792f661c7c9bc4b
Leo Lamprecht <mindrun@icloud.com>
2017-05-26 16:05:33 +0200

    Proper loading animation

--- renderer/components/feed/events/loading.js
+++ renderer/components/feed/events/loading.js
@@ -2,14 +2,20 @@
 import React from 'react'
 
 const Loading = () => (
-  <div>
-    <figure>
-      <figcaption>Loading Events...</figcaption>
-    </figure>
+  <aside>
+    <section>
+      <span>
+        <b />
+        <b />
+        <b />
+      </span>
+
+      <p>Loading Events...</p>
+    </section>
 
     <style jsx>
       {`
-      div {
+      aside {
         display: flex;
         width: 100%;
         height: 100%;
@@ -19,9 +25,58 @@ const Loading = () => (
         justify-content: center;
         flex-direction: column;
       }
+
+      p {
+        margin: 15px 0 0 0;
+        color: #999;
+        font-weight: 500;
+      }
+
+      span {
+        margin: 0 auto 0 auto;
+        width: 70px;
+        text-align: center;
+        display: block;
+      }
+
+      span b {
+        width: 13px;
+        height: 13px;
+        background-color: #999999;
+        margin: 0 3px;
+        border-radius: 100%;
+        display: inline-block;
+        -webkit-animation: sbouncedelay 1.4s infinite ease-in-out both;
+        animation: bouncedelay 1.4s infinite ease-in-out both;
+      }
+
+      span b:nth-child(1) {
+        -webkit-animation-delay: -0.32s;
+        animation-delay: -0.32s;
+      }
+
+      span b:nth-child(2) {
+        -webkit-animation-delay: -0.16s;
+        animation-delay: -0.16s;
+      }
+
+      @-webkit-keyframes bouncedelay {
+        0%, 80%, 100% { -webkit-transform: scale(0) }
+        40% { -webkit-transform: scale(1.0) }
+      }
+
+      @keyframes bouncedelay {
+        0%, 80%, 100% {
+          -webkit-transform: scale(0);
+          transform: scale(0);
+        } 40% {
+          -webkit-transform: scale(1.0);
+          transform: scale(1.0);
+        }
+      }
     `}
     </style>
-  </div>
+  </aside>
 )
 
 export default Loading
