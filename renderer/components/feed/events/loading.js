77bf2072bc0a364284c23c2da483cd144be88b3c
Leo Lamprecht <mindrun@icloud.com>
2017-05-29 20:45:10 +0200

    Use native loading indicator
    This closes #25

--- renderer/components/feed/events/loading.js
+++ renderer/components/feed/events/loading.js
@@ -4,12 +4,7 @@ import React from 'react'
 const Loading = () => (
   <aside>
     <section>
-      <span>
-        <b />
-        <b />
-        <b />
-      </span>
-
+      <img src="/static/loading.gif" />
       <p>Loading Events...</p>
     </section>
 
@@ -26,53 +21,16 @@ const Loading = () => (
         flex-direction: column;
       }
 
-      p {
-        margin: 15px 0 0 0;
-        color: #999;
-        font-size: 16px;
-      }
-
-      span {
-        margin: 0 auto 0 auto;
-        width: 70px;
-        text-align: center;
+      img {
+        width: 30px;
+        margin: 0 auto;
         display: block;
       }
 
-      span b {
-        width: 13px;
-        height: 13px;
-        background-color: #999999;
-        margin: 0 3px;
-        border-radius: 100%;
-        display: inline-block;
-        -webkit-animation: sbouncedelay 1.4s infinite ease-in-out both;
-        animation: bouncedelay 1.4s infinite ease-in-out both;
-      }
-
-      span b:nth-child(1) {
-        -webkit-animation-delay: -0.32s;
-        animation-delay: -0.32s;
-      }
-
-      span b:nth-child(2) {
-        -webkit-animation-delay: -0.16s;
-        animation-delay: -0.16s;
-      }
-
-      @-webkit-keyframes bouncedelay {
-        0%, 80%, 100% { -webkit-transform: scale(0) }
-        40% { -webkit-transform: scale(1.0) }
-      }
-
-      @keyframes bouncedelay {
-        0%, 80%, 100% {
-          -webkit-transform: scale(0);
-          transform: scale(0);
-        } 40% {
-          -webkit-transform: scale(1.0);
-          transform: scale(1.0);
-        }
+      p {
+        margin: 10px 0 0 0;
+        color: #999;
+        font-size: 13px;
       }
     `}
     </style>
