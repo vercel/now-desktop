fdc61df9718df1d426339976e044e8044e34a55f
Leo Lamprecht <mindrun@icloud.com>
2017-05-31 20:09:59 +0200

    Restart app after uncaught exception occured

--- main/utils/exception.js
+++ main/utils/exception.js
@@ -7,8 +7,10 @@ const serializeError = require('serialize-error')
 const fetch = require('node-fetch')
 
 module.exports = async error => {
+  // Make the error sendable using GET
   const errorParts = serializeError(error)
 
+  // Prepare the request query
   const query = queryString.stringify({
     sender: 'Now Desktop',
     name: errorParts.name,
@@ -16,9 +18,13 @@ module.exports = async error => {
     stack: errorParts.stack
   })
 
+  // Post the error to slack
   try {
     await fetch('https://errors.zeit.sh/?' + query)
   } catch (err) {}
 
-  app.quit()
+  // Restart the app, so that it doesn't continue
+  // running in a broken state
+  app.relaunch()
+  app.exit(0)
 }
