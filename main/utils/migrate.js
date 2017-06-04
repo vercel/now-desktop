6272cd1efcc8f1df4def2c14955db3abceb02d20
Leo Lamprecht <mindrun@icloud.com>
2017-06-04 14:20:35 +0200

    Migrate config using existing method

--- main/utils/migrate.js
+++ main/utils/migrate.js
@@ -1,60 +1,8 @@
-// Packages
-const fetch = require('node-fetch')
-
 // Utilities
 const logout = require('../utils/logout')
 const { getConfig, saveConfig } = require('./config')
-
-const NETWORK_ERR_CODE = 'network_error'
-const NETWORK_ERR_MESSAGE = 'A network error has occurred. Please retry'
-const API_USER = 'api/www/user'
-
-const load = async (path, token) => {
-  const headers = {}
-  const url = `https://zeit.co/${path}`
-
-  // On login, the token isn't saved to the config yet
-  // but we need to make another network request to
-  // get all the user details before we can save to config there
-  headers.Authorization = `bearer ${token}`
-
-  let res
-  let data
-  let error
-
-  try {
-    res = await fetch(url, { headers })
-    if (res.status < 200 || res.status >= 300) {
-      if (res.headers.get('Content-Type') === 'application/json') {
-        data = await res.json()
-
-        // Remove this hack https://github.com/zeit/front/issues/553
-        error = new Error(data.message ? data.message : data.error.message)
-        error.res = res
-        error.status = res.status
-
-        // Remove this hack https://github.com/zeit/front/issues/553
-        error.code = data.error ? data.error.code : res.status
-      } else {
-        // Handle it below as network error
-        throw new Error()
-      }
-    } else {
-      data = await res.json()
-    }
-  } catch (err) {
-    error = new Error(NETWORK_ERR_MESSAGE + ` (${url})`)
-    error.code = NETWORK_ERR_CODE
-    error.res = null
-    error.status = null
-  }
-
-  if (error) {
-    throw error
-  }
-
-  return data
-}
+const loadData = require('./data/load')
+const { API_USER } = require('./data/endpoints')
 
 const required = ['uid', 'email', 'username']
 
@@ -88,7 +36,7 @@ module.exports = async () => {
   let data
 
   try {
-    data = await load(API_USER, config.token)
+    data = await loadData(API_USER, config.token)
   } catch (err) {
     await logout()
     return
