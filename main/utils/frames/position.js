53d4650901a4c940a06027ae2a3816b2c9cddfe3
Leo Lamprecht <mindrun@icloud.com>
2017-06-05 16:58:16 +0200

    Ensured that window is correctly positioned

--- main/utils/frames/position.js
+++ main/utils/frames/position.js
@@ -1,7 +1,11 @@
+// Native
+const { platform } = require('os')
+
 let trayBoundsCache = null
 
 module.exports = (tray, window) => {
   const trayBounds = tray.getBounds()
+  const isWin = platform() === 'win32'
 
   if (trayBoundsCache) {
     // Compare only the object props
@@ -17,25 +21,30 @@ module.exports = (tray, window) => {
   // I don't know why, but that's required by electron
   const { screen } = require('electron')
 
-  const windowWidth = window.getSize()[0]
+  const windowSize = window.getSize()
   const trayCenter = trayBounds.x + trayBounds.width / 2
 
-  let horizontalPosition = trayCenter - windowWidth / 2
-  const verticalPosition = trayBounds.height + 6
+  let horizontalPosition = trayCenter - windowSize[0] / 2
+  let verticalPosition = trayBounds.height + 6
 
   // Find the current display
   const display = screen.getDisplayMatching(trayBounds)
 
   if (display) {
-    const displayWidth = display.workAreaSize.width
+    const displayArea = display.workAreaSize
 
-    const left = horizontalPosition + windowWidth
-    const maxLeft = displayWidth - 18
+    const left = horizontalPosition + windowSize[0]
+    const maxLeft = displayArea.width - (isWin ? 25 : 18)
 
     // Check if window would be outside screen
     // If yes, make sure it isn't
     if (left > maxLeft) {
       horizontalPosition -= left - maxLeft
+    }
+
+    if (isWin) {
+      const offset = trayBounds.height + windowSize[1] - 25
+      verticalPosition = displayArea.height - offset
     }
   }
 
