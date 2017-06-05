cd6cae5ab9d6c2496849dbf880c3e9261cb89321
Leo Lamprecht <mindrun@icloud.com>
2017-06-05 16:20:46 +0200

    No container needed anymore

--- renderer/pages/about.js
+++ renderer/pages/about.js
@@ -12,7 +12,6 @@ import CloseWindowSVG from '../vectors/close-window'
 import UpdatedSVG from '../vectors/updated'
 
 // Components
-import Container from '../components/container'
 import Licenses from '../components/about/licenses'
 
 // Helpers
@@ -179,239 +178,252 @@ class About extends Component {
     const appVersion = getAppVersion()
 
     return (
-      <Container>
-        <div>
-          {platform() === 'win32' &&
-            <div className="window-controls">
-              <span onClick={this.handleCloseClick.bind(this)}>
-                <CloseWindowSVG />
-              </span>
-            </div>}
-          <section className="wrapper">
-            <span className="window-title">About</span>
-
-            <img src="/static/app-icon.png" />
-
-            <h1>Now</h1>
-            <h2>
-              Version
-              {' '}
-              {appVersion ? <b>{appVersion}</b> : ''}
-              {' '}
-              {this.state && this.state.lastReleaseDate}
-            </h2>
-
-            {this.updateStatus.apply(this)}
-
-            <article>
-              <h1>Authors</h1>
-
-              <p>
-                <a href="https://twitter.com/notquiteleo" onClick={openLink}>
-                  Leo Lamprecht
-                </a>
-                <br />
-                <a href="https://twitter.com/evilrabbit_" onClick={openLink}>
-                  Evil Rabbit
-                </a>
-                <br />
-                <a href="https://twitter.com/rauchg" onClick={openLink}>
-                  Guillermo Rauch
-                </a>
-                <br />
-                <a href="https://twitter.com/matheusfrndes" onClick={openLink}>
-                  Matheus Fernandes
-                </a>
-              </p>
-
-              <h1>{'3rd party software'}</h1>
-              <Licenses />
-            </article>
-
-            <span className="copyright">
-              Made by <a href="https://zeit.co" onClick={openLink}>ZEIT</a>
+      <div>
+        {platform() === 'win32' &&
+          <div className="window-controls">
+            <span onClick={this.handleCloseClick.bind(this)}>
+              <CloseWindowSVG />
             </span>
+          </div>}
+        <section className="wrapper">
+          <span className="window-title">About</span>
 
-            <nav>
-              <a href="https://zeit.co/docs" onClick={openLink}>Docs</a>
-              <a href="https://github.com/zeit/now-desktop" onClick={openLink}>
-                Source
+          <img src="/static/app-icon.png" />
+
+          <h1>Now</h1>
+          <h2>
+            Version
+            {' '}
+            {appVersion ? <b>{appVersion}</b> : ''}
+            {' '}
+            {this.state && this.state.lastReleaseDate}
+          </h2>
+
+          {this.updateStatus.apply(this)}
+
+          <article>
+            <h1>Authors</h1>
+
+            <p>
+              <a href="https://twitter.com/notquiteleo" onClick={openLink}>
+                Leo Lamprecht
               </a>
-              <a onClick={this.handleTutorial.bind(this)}>Tutorial</a>
-            </nav>
-          </section>
+              <br />
+              <a href="https://twitter.com/evilrabbit_" onClick={openLink}>
+                Evil Rabbit
+              </a>
+              <br />
+              <a href="https://twitter.com/rauchg" onClick={openLink}>
+                Guillermo Rauch
+              </a>
+              <br />
+              <a href="https://twitter.com/matheusfrndes" onClick={openLink}>
+                Matheus Fernandes
+              </a>
+            </p>
 
-          <style jsx>
-            {`
-            div {
-              background: #ECECEC;
-              height: 100vh;
-            }
+            <h1>{'3rd party software'}</h1>
+            <Licenses />
+          </article>
 
-            .window-controls {
-              display: flex;
-              justify-content: flex-end;
-              position: fixed;
-              right: 0;
-              top: 0;
-              left: 0;
-              height: 10px;
-              padding: 10px;
-              z-index: 5000; /* the slick arrow is at 4000 */
-              background: transparent;
-            }
+          <span className="copyright">
+            Made by <a href="https://zeit.co" onClick={openLink}>ZEIT</a>
+          </span>
 
-            .window-controls span {
-              opacity: .5;
-              font-size: 0;
-              display: block;
-              -webkit-app-region: no-drag;
-              margin-left: 10px;
-            }
+          <nav>
+            <a href="https://zeit.co/docs" onClick={openLink}>Docs</a>
+            <a href="https://github.com/zeit/now-desktop" onClick={openLink}>
+              Source
+            </a>
+            <a onClick={this.handleTutorial.bind(this)}>Tutorial</a>
+          </nav>
+        </section>
 
-            .window-controls span:hover {
-              opacity: 1;
-            }
+        <style jsx>
+          {`
+          div {
+            background: #ECECEC;
+            height: 100vh;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Helvetica Neue, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -webkit-app-region: drag;
+          }
 
-            a {
-              -webkit-app-region: no-drag;
-            }
+          .window-controls {
+            display: flex;
+            justify-content: flex-end;
+            position: fixed;
+            right: 0;
+            top: 0;
+            left: 0;
+            height: 10px;
+            padding: 10px;
+            z-index: 5000; /* the slick arrow is at 4000 */
+            background: transparent;
+          }
 
-            .wrapper {
-              text-align: center;
-              padding-top: 40px;
-              color: #434343;
-            }
+          .window-controls span {
+            opacity: .5;
+            font-size: 0;
+            display: block;
+            -webkit-app-region: no-drag;
+            margin-left: 10px;
+          }
 
-            img {
-              width: 100px;
-            }
+          .window-controls span:hover {
+            opacity: 1;
+          }
 
-            h1,
-            h2 {
-              margin: 0;
-            }
+          a {
+            -webkit-app-region: no-drag;
+          }
 
-            h1 {
-              font-size: 15px;
-              font-weight: 700;
-              margin: 5px 0 15px 0;
-            }
+          .wrapper {
+            text-align: center;
+            padding-top: 40px;
+            color: #434343;
+          }
 
-            h2 {
-              font-size: 12px;
-              font-weight: 400;
-              cursor: default;
-            }
+          img {
+            width: 100px;
+          }
 
-            h2 span {
-              color: #5319e7;
-              cursor: pointer;
-            }
+          h1,
+          h2 {
+            margin: 0;
+          }
 
-            .window-title {
-              font-size: 12px;
-              color: #434343;
-              text-align: center;
-              position: fixed;
-              top: 0;
-              left: 0;
-              right: 0;
-              height: 37px;
-              line-height: 37px;
-            }
+          h1 {
+            font-size: 15px;
+            font-weight: 700;
+            margin: 5px 0 15px 0;
+          }
 
-            article {
-              background: #fff;
-              border-top: 1px solid #BFBFBF;
-              border-bottom: 1px solid #BFBFBF;
-              height: 135px;
-              text-align: left;
-              padding: 15px;
-              box-sizing: border-box;
-              overflow-y: scroll;
-              -webkit-app-region: no-drag;
-              -webkit-user-select: text;
-              margin-top: 20px;
-            }
+          h2 {
+            font-size: 12px;
+            font-weight: 400;
+            cursor: default;
+          }
 
-            article h1,
-            article p {
-              color: #707070;
-              font-size: 11px;
-            }
+          h2 span {
+            color: #5319e7;
+            cursor: pointer;
+          }
 
-            article h1 {
-              text-transform: uppercase;
-              margin-top: 15px;
-            }
+          .window-title {
+            font-size: 12px;
+            color: #434343;
+            text-align: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            height: 37px;
+            line-height: 37px;
+          }
 
-            article h1:first-child {
-              margin-top: 0;
-            }
+          article {
+            background: #fff;
+            border-top: 1px solid #BFBFBF;
+            border-bottom: 1px solid #BFBFBF;
+            height: 135px;
+            text-align: left;
+            padding: 15px;
+            box-sizing: border-box;
+            overflow-y: scroll;
+            -webkit-app-region: no-drag;
+            -webkit-user-select: text;
+            margin-top: 20px;
+          }
 
-            article p {
-              line-height: 19px;
-            }
+          article h1,
+          article p {
+            color: #707070;
+            font-size: 11px;
+          }
 
-            article a {
-              color: #707070;
-              text-decoration: none;
-            }
+          article h1 {
+            text-transform: uppercase;
+            margin-top: 15px;
+          }
 
-            article a:hover {
-              color: #2b2b2b;
-            }
+          article h1:first-child {
+            margin-top: 0;
+          }
 
-            .copyright {
-              font-size: 11px;
-              margin-top: 11px;
-              display: block;
-            }
+          article p {
+            line-height: 19px;
+          }
 
-            .copyright a {
-              color: inherit;
-              text-decoration: none;
-            }
+          article a {
+            color: #707070;
+            text-decoration: none;
+          }
 
-            .copyright a:before {
-              content: '\\25B2';
-            }
+          article a:hover {
+            color: #2b2b2b;
+          }
 
-            .copyright a:hover {
-              color: #000;
-            }
+          .copyright {
+            font-size: 11px;
+            margin-top: 11px;
+            display: block;
+          }
 
-            nav a {
-              font-size: 11px;
-              color: #434343;
-              text-decoration: none;
-              padding: 0 10px;
-              position: relative;
-              cursor: pointer;
-            }
+          .copyright a {
+            color: inherit;
+            text-decoration: none;
+          }
 
-            nav a:after {
-              content: '';
-              position: absolute;
-              right: 0;
-              width: 1px;
-              height: 10px;
-              background: #434343;
-              top: 2px;
-            }
+          .copyright a:before {
+            content: '\\25B2';
+          }
 
-            nav a:hover {
-              color: #000;
-            }
+          .copyright a:hover {
+            color: #000;
+          }
 
-            nav a:last-child:after {
-              display: none;
-            }
-          `}
-          </style>
-        </div>
-      </Container>
+          nav a {
+            font-size: 11px;
+            color: #434343;
+            text-decoration: none;
+            padding: 0 10px;
+            position: relative;
+            cursor: pointer;
+          }
+
+          nav a:after {
+            content: '';
+            position: absolute;
+            right: 0;
+            width: 1px;
+            height: 10px;
+            background: #434343;
+            top: 2px;
+          }
+
+          nav a:hover {
+            color: #000;
+          }
+
+          nav a:last-child:after {
+            display: none;
+          }
+        `}
+        </style>
+
+        <style jsx global>
+          {`
+          body {
+            margin: 0;
+          }
+
+          ::selection {
+            background: #A7D8FF;
+          }
+        `}
+        </style>
+      </div>
     )
   }
 }
--- renderer/pages/tutorial.js
+++ renderer/pages/tutorial.js
@@ -19,7 +19,6 @@ import LogoSVG from '../vectors/logo'
 import Title from '../components/title'
 import Login from '../components/tutorial/login'
 import Binary from '../components/tutorial/binary'
-import Container from '../components/container'
 
 const SliderArrows = props => {
   const properties = Object.assign({}, props)
@@ -370,13 +369,12 @@ class Sections extends Component {
 }
 
 const Tutorial = () =>
-  <Container>
-    <main>
-      <Title>Welcome to Now</Title>
-      <Sections />
+  <main>
+    <Title>Welcome to Now</Title>
+    <Sections />
 
-      <style jsx>
-        {`
+    <style jsx>
+      {`
         main {
           color: #fff;
           background: #000;
@@ -384,192 +382,202 @@ const Tutorial = () =>
           width: 100vw;
         }
       `}
-      </style>
+    </style>
 
-      <style jsx global>
-        {`
+    <style jsx global>
+      {`
+          body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Helvetica Neue, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -webkit-app-region: drag;
+          }
+
+          ::selection {
+            background: #A7D8FF;
+          }
+
           .slick-slider {
-          position: relative;
-          display: block;
-          box-sizing: border-box;
-          user-select: none;
-          -webkit-touch-callout: none;
-          touch-action: pan-y;
-          -webkit-tap-highlight-color: transparent;
-        }
+            position: relative;
+            display: block;
+            box-sizing: border-box;
+            user-select: none;
+            -webkit-touch-callout: none;
+            touch-action: pan-y;
+            -webkit-tap-highlight-color: transparent;
+          }
 
-        .slick-list {
-          position: relative;
-          display: block;
-          overflow: hidden;
-          margin: 0;
-          padding: 0;
-        }
+          .slick-list {
+            position: relative;
+            display: block;
+            overflow: hidden;
+            margin: 0;
+            padding: 0;
+          }
 
-        .slick-list:focus {
-          outline: none;
-        }
+          .slick-list:focus {
+            outline: none;
+          }
 
-        .slick-list.dragging {
-          cursor: pointer;
-          cursor: hand;
-        }
+          .slick-list.dragging {
+            cursor: pointer;
+            cursor: hand;
+          }
 
-        .slick-slider .slick-track,
-        .slick-slider .slick-list {
-          transform: translate3d(0, 0, 0);
-        }
+          .slick-slider .slick-track,
+          .slick-slider .slick-list {
+            transform: translate3d(0, 0, 0);
+          }
 
-        .slick-track {
-          position: relative;
-          top: 0;
-          left: 0;
-          display: block;
-        }
+          .slick-track {
+            position: relative;
+            top: 0;
+            left: 0;
+            display: block;
+          }
 
-        .slick-track:before,
-        .slick-track:after {
-          display: table;
-          content: '';
-        }
+          .slick-track:before,
+          .slick-track:after {
+            display: table;
+            content: '';
+          }
 
-        .slick-track:after {
-          clear: both;
-        }
+          .slick-track:after {
+            clear: both;
+          }
 
-        .slick-loading .slick-track {
-          visibility: hidden;
-        }
+          .slick-loading .slick-track {
+            visibility: hidden;
+          }
 
-        .slick-slide {
-          display: none;
-          float: left;
-          height: 100%;
-          min-height: 1px;
-        }
+          .slick-slide {
+            display: none;
+            float: left;
+            height: 100%;
+            min-height: 1px;
+          }
 
-        [dir='rtl'] .slick-slide {
-          float: right;
-        }
+          [dir='rtl'] .slick-slide {
+            float: right;
+          }
 
-        .slick-slide img {
-          display: block;
-        }
+          .slick-slide img {
+            display: block;
+          }
 
-        .slick-slide.slick-loading img {
-          display: none;
-        }
+          .slick-slide.slick-loading img {
+            display: none;
+          }
 
-        .slick-slide.dragging img {
-          pointer-events: none;
-        }
+          .slick-slide.dragging img {
+            pointer-events: none;
+          }
 
-        .slick-initialized .slick-slide {
-          display: block;
-        }
+          .slick-initialized .slick-slide {
+            display: block;
+          }
 
-        .slick-loading .slick-slide {
-          visibility: hidden;
-        }
+          .slick-loading .slick-slide {
+            visibility: hidden;
+          }
 
-        .slick-vertical .slick-slide {
-          display: block;
-          height: auto;
-          border: 1px solid transparent;
-        }
+          .slick-vertical .slick-slide {
+            display: block;
+            height: auto;
+            border: 1px solid transparent;
+          }
 
-        .slick-arrow.slick-hidden {
-          display: none;
-        }
+          .slick-arrow.slick-hidden {
+            display: none;
+          }
 
-        .slick-initialized .slick-slide {
-          height: 100vh;
-          justify-content: center;
-          align-items: center;
-          display: flex;
-          flex-direction: column;
-          width: 100%;
-        }
+          .slick-initialized .slick-slide {
+            height: 100vh;
+            justify-content: center;
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+          }
 
-        .slick-arrow {
-          height: 100vh !important;
-          z-index: 4000;
-          top: 0;
-          position: fixed;
-          width: 50px !important;
-          display: flex !important;
-          justify-content: center;
-          align-items: center;
-          background: linear-gradient(to left, #000, transparent);
-          cursor: pointer;
-          opacity: 0;
-          transition: opacity .3s ease;
-          -webkit-app-region: no-drag;
-        }
+          .slick-arrow {
+            height: 100vh !important;
+            z-index: 4000;
+            top: 0;
+            position: fixed;
+            width: 50px !important;
+            display: flex !important;
+            justify-content: center;
+            align-items: center;
+            background: linear-gradient(to left, #000, transparent);
+            cursor: pointer;
+            opacity: 0;
+            transition: opacity .3s ease;
+            -webkit-app-region: no-drag;
+          }
 
-        .slick-arrow:not(.slick-disabled) {
-          opacity: .5 !important;
-        }
+          .slick-arrow:not(.slick-disabled) {
+            opacity: .5 !important;
+          }
 
-        .slick-arrow:not(.slick-disabled):hover {
-          opacity: 1 !important;
-        }
+          .slick-arrow:not(.slick-disabled):hover {
+            opacity: 1 !important;
+          }
 
-        .slick-arrow.slick-prev {
-          left: 0;
-          transform: rotate(180deg);
-        }
+          .slick-arrow.slick-prev {
+            left: 0;
+            transform: rotate(180deg);
+          }
 
-        .slick-arrow.slick-next {
-          right: 0;
-        }
+          .slick-arrow.slick-next {
+            right: 0;
+          }
 
-        .slick-dots {
-          margin: 0;
-          padding: 0;
-          position: fixed;
-          display: flex !important;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          height: 60px;
-          justify-content: center;
-          align-items: center;
-          list-style: none;
-          z-index: 100;
-        }
+          .slick-dots {
+            margin: 0;
+            padding: 0;
+            position: fixed;
+            display: flex !important;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            height: 60px;
+            justify-content: center;
+            align-items: center;
+            list-style: none;
+            z-index: 100;
+          }
 
-        .slick-dots li {
-          display: block;
-          -webkit-app-region: no-drag;
-          margin: 0 4px;
-        }
+          .slick-dots li {
+            display: block;
+            -webkit-app-region: no-drag;
+            margin: 0 4px;
+          }
 
-        .slick-dots li button {
-          display: block;
-          height: 10px;
-          width: 10px;
-          background: #fff;
-          border: 0;
-          text-indent: -999px;
-          border-radius: 100%;
-          padding: 0;
-          opacity: .5;
-          cursor: pointer;
-          transition: opacity .4s;
-        }
+          .slick-dots li button {
+            display: block;
+            height: 10px;
+            width: 10px;
+            background: #fff;
+            border: 0;
+            text-indent: -999px;
+            border-radius: 100%;
+            padding: 0;
+            opacity: .5;
+            cursor: pointer;
+            transition: opacity .4s;
+          }
 
-        .slick-dots li button:focus {
-          outline: 0;
-        }
+          .slick-dots li button:focus {
+            outline: 0;
+          }
 
-        .slick-dots li button:hover,
-        .slick-dots li.slick-active button {
-          opacity: 1;
-        }
+          .slick-dots li button:hover,
+          .slick-dots li.slick-active button {
+            opacity: 1;
+          }
       `}
-      </style>
-    </main>
-  </Container>
+    </style>
+  </main>
 
 export default Tutorial
