a4d6a60f98de5e5af793ad7dadd378cf64720e34
Leo Lamprecht <mindrun@icloud.com>
2017-06-13 14:06:17 +0200

    Only keep JSX inside the tutorial slides

--- renderer/components/tutorial/slides/intro.js
+++ renderer/components/tutorial/slides/intro.js
@@ -19,7 +19,7 @@ class Intro extends PureComponent {
     }
   }
 
-  renderDescription() {
+  render() {
     const { sendingMail, security, done } = this.state
 
     if (sendingMail) {
@@ -70,57 +70,6 @@ class Intro extends PureComponent {
 
         <style jsx>{introStyles}</style>
       </article>
-    )
-  }
-
-  render() {
-    return (
-      <div>
-        {this.renderDescription()}
-
-        <style jsx global>
-          {`
-            .sending-status i {
-              font-weight: 700;
-              font-style: normal;
-              animation-name: blink;
-              animation-duration: 1.4s;
-              animation-iteration-count: infinite;
-              animation-fill-mode: both;
-              font-size: 150%;
-            }
-            .sending-status i:nth-child(3) {
-              animation-delay: .2s;
-            }
-            .sending-status i:nth-child(4) {
-              animation-delay: .4s;
-            }
-            .security-token {
-              display: block;
-              margin-top: 35px;
-              background: #CCCCCC;
-              color: #000;
-              border-radius: 3px;
-              font-weight: normal;
-              padding: 10px;
-              font-weight: 700;
-              font-size: 13px;
-              letter-spacing: 0.15em;
-            }
-            @keyframes blink {
-              0% {
-                opacity: 0.1;
-              }
-              20% {
-                opacity: 1;
-              }
-              100% {
-                opacity: .2;
-              }
-            }
-          `}
-        </style>
-      </div>
     )
   }
 }
--- renderer/styles/intro.js
+++ renderer/styles/intro.js
@@ -14,4 +14,49 @@ export default `
     max-width: 420px;
     text-align: center;
   }
+
+  .sending-status i {
+    font-weight: 700;
+    font-style: normal;
+    animation-name: blink;
+    animation-duration: 1.4s;
+    animation-iteration-count: infinite;
+    animation-fill-mode: both;
+    font-size: 150%;
+  }
+
+  .sending-status i:nth-child(3) {
+    animation-delay: .2s;
+  }
+
+  .sending-status i:nth-child(4) {
+    animation-delay: .4s;
+  }
+
+  .security-token {
+    display: block;
+    margin-top: 35px;
+    background: #CCCCCC;
+    color: #000;
+    border-radius: 3px;
+    font-weight: normal;
+    padding: 10px;
+    font-weight: 700;
+    font-size: 13px;
+    letter-spacing: 0.15em;
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 0.1;
+    }
+
+    20% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: .2;
+    }
+  }
 `
