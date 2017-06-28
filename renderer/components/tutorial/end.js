7df47d17f09dca05de83109d6c85c772088c6819
Leo Lamprecht <mindrun@icloud.com>
2017-06-28 20:50:26 +0200

    Implemented the same for the tutorial end

--- renderer/components/tutorial/end.js
+++ renderer/components/tutorial/end.js
@@ -1,5 +1,6 @@
 // Packages
-import React from 'react'
+import electron from 'electron'
+import React, { Component } from 'react'
 
 // Styles
 import introStyles from '../../styles/components/tutorial/intro'
@@ -7,18 +8,43 @@ import introStyles from '../../styles/components/tutorial/intro'
 // Components
 import Button from './button'
 
-const End = () =>
-  <article>
-    <p><b>That was the tutorial!</b></p>
+class End extends Component {
+  constructor(props) {
+    super(props)
 
-    <p className="has-mini-spacing">
-      Are you ready to
-      deploy something now? If so, simply click the
-      button below to view the event feed:
-    </p>
+    this.remote = electron.remote || false
+    this.showApp = this.showApp.bind(this)
+  }
 
-    <Button>Get Started</Button>
-    <style jsx>{introStyles}</style>
-  </article>
+  showApp(event) {
+    event.preventDefault()
+
+    if (!this.remote) {
+      return
+    }
+
+    const currentWindow = this.remote.getCurrentWindow()
+
+    // Show the event feed
+    currentWindow.emit('open-tray')
+  }
+
+  render() {
+    return (
+      <article>
+        <p><b>That was the tutorial!</b></p>
+
+        <p className="has-mini-spacing">
+          Are you ready to
+          deploy something now? If so, simply click the
+          button below to view the event feed:
+        </p>
+
+        <Button onClick={this.showApp}>Get Started</Button>
+        <style jsx>{introStyles}</style>
+      </article>
+    )
+  }
+}
 
 export default End
