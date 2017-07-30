74114ee251f192b875843b9619307714026a6a2e
Leo Lamprecht <mindrun@icloud.com>
2017-07-30 12:34:32 +0200

    Fixed typo

--- main/updates.js
+++ main/updates.js
@@ -132,7 +132,7 @@ const startAppUpdates = () => {
   }
 
   // Check for updates every 15 minutes
-  // We won't to this an launch because it might
+  // We won't do this an launch because it might
   // trigger an error loop, since the app is
   // able to quit and install by itself
   setInterval(checkForUpdates, ms('15m'))
