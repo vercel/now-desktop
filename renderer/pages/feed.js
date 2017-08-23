f1ba01e7b269f822297224cb8e8890b15263e4bc
Leo Lamprecht <mindrun@icloud.com>
2017-08-23 19:23:05 +0200

    Ensure that always enough events are rendered

--- renderer/pages/feed.js
+++ renderer/pages/feed.js
@@ -508,7 +508,7 @@ class Feed extends React.Component {
     for (const group of groups) {
       const { length } = this.filterEvents(scopedEvents, relatedTeam, group)
 
-      if (length > 30) {
+      if (length > 10) {
         continue
       }
 
