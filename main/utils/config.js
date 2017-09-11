28d75c20d5744437674a451235f0bdd41c2a9b2e
Leo Lamprecht <mindrun@icloud.com>
2017-09-11 19:26:22 +0200

    Added button

--- main/utils/config.js
+++ main/utils/config.js
@@ -9,9 +9,6 @@ const groom = require('groom')
 const deepExtend = require('deep-extend')
 const { watch } = require('chokidar')
 
-// Utilities
-const notify = require('../notify')
-
 const paths = {
   auth: '.now/auth.json',
   config: '.now/config.json',
@@ -212,14 +209,5 @@ exports.watchConfig = async () => {
   configWatcher.on('change', () => configChanged(logout))
 
   // Log out when a config file is removed
-  configWatcher.on('unlink', () => {
-    // But before doing so, post a notification
-    notify({
-      title: 'Logged Out',
-      body:
-        'You were logged out from Now Desktop because you logged out from Now CLI.'
-    })
-
-    logout()
-  })
+  configWatcher.on('unlink', () => logout('config-removed'))
 }
