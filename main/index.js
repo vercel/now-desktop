7125622a5e2a97aafecb85f8993b7033685e7b64
Chris Broome <chrisbroome7@gmail.com>
2018-01-24 00:17:24 -0500

    Fix issues on linux
    - Use white icon
    - Set context menu for tray icon
    - Add "Main" context menu item to toggle the main window

--- main/index.js
+++ main/index.js
@@ -134,7 +134,10 @@ app.on('ready', async () => {
 
   // I have no idea why, but path.resolve doesn't work here
   try {
-    const iconName = process.platform === 'win32' ? 'iconWhite' : 'iconTemplate'
+    const iconName =
+      process.platform === 'win32'
+        ? 'iconWhite'
+        : process.platform === 'linux' ? 'iconWhite' : 'iconTemplate'
     tray = new electron.Tray(resolvePath(`./main/static/tray/${iconName}.png`))
   } catch (err) {
     handleException(err)
@@ -222,6 +225,11 @@ app.on('ready', async () => {
   tray.on('drop-files', filesDropped)
   tray.on('click', toggleActivity)
   tray.on('double-click', toggleActivity)
+
+  // Linux requires setContextMenu to be called in order for the context menu to populate correctly
+  tray.setContextMenu(
+    loggedIn ? await contextMenu(windows) : outerMenu(app, windows)
+  )
 
   let submenuShown = false
 
