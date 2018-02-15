239169fe634911c1ddd04ba7407dec41719315ac
Leo Lamprecht <mindrun@icloud.com>
2018-02-14 22:40:08 -0800

    Sweet message for OSS deployments

--- main/utils/deploy/oss-prompt.js
+++ main/utils/deploy/oss-prompt.js
@@ -1,5 +1,5 @@
 // Packages
-const { dialog, nativeImage } = require('electron')
+const { dialog, nativeImage, shell } = require('electron')
 const { resolve } = require('app-root-path')
 
 module.exports = () => {
@@ -8,14 +8,23 @@ module.exports = () => {
 
   const response = dialog.showMessageBox({
     type: 'question',
-    message: `You're on the OSS Plan`,
+    message: `You Are on the OSS Plan`,
     detail:
-      'This means that your code and logs will be made public. Do you really want to deploy?',
-    buttons: ['Yes', 'No'],
+      'This means that your code and logs will be publicly accessible. ' +
+      'Do you really want to deploy?' +
+      '\n\n' +
+      'To prevent this message from showing up, upgrade to ' +
+      'a higher plan using the button on the left.',
+    buttons: ['Deploy', 'Cancel', 'Upgrade'],
     defaultId: 0,
     cancelId: 1,
     icon
   })
+
+  if (response === 2) {
+    shell.openExternal('https://zeit.co/account/plan')
+    return false
+  }
 
   return response === 0
 }
