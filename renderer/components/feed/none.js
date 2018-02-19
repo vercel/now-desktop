de7b1e8ba763c72b1d40d592261a4131729aad56
Leo Lamprecht <mindrun@icloud.com>
2018-02-19 11:51:03 -0800

    Fixed error page for search (#444)

--- renderer/components/feed/none.js
+++ renderer/components/feed/none.js
@@ -12,16 +12,16 @@ const NoEvents = ({ filtered }) => (
   <div>
     {filtered ? (
       <Fragment>
-        <h1 key="heading">No Events Available</h1>,
-        <p key="description">
+        <h1>No Events Available</h1>
+        <p>
           There are no events matching your search pattern. Try a different
-          wording!
+          wording.
         </p>
       </Fragment>
     ) : (
       <Fragment>
-        <h1 key="heading">No Events Found</h1>,
-        <p key="description">
+        <h1>No Events Found</h1>
+        <p>
           You can pick a different category of events using the{' '}
           <FilterIcon background="#F5F5F5" /> filter on the top.
         </p>
