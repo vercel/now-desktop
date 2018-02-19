1abe03e424ba1e0036e6e836730f1dc6d611db46
Leo Lamprecht <mindrun@icloud.com>
2018-02-18 21:58:21 -0800

    Use fragment

--- renderer/components/feed/none.js
+++ renderer/components/feed/none.js
@@ -1,4 +1,5 @@
 // Packages
+import { Fragment } from 'react'
 import { bool } from 'prop-types'
 
 // Styles
@@ -9,21 +10,23 @@ import FilterIcon from '../../vectors/filter'
 
 const NoEvents = ({ filtered }) => (
   <div>
-    {filtered
-      ? [
-          <h1 key="heading">No Events Available</h1>,
-          <p key="description">
-            There are no events matching your search pattern. Try a different
-            wording!
-          </p>
-        ]
-      : [
-          <h1 key="heading">No Events Found</h1>,
-          <p key="description">
-            You can pick a different category of events using the{' '}
-            <FilterIcon background="#F5F5F5" /> filter on the top.
-          </p>
-        ]}
+    {filtered ? (
+      <Fragment>
+        <h1 key="heading">No Events Available</h1>,
+        <p key="description">
+          There are no events matching your search pattern. Try a different
+          wording!
+        </p>
+      </Fragment>
+    ) : (
+      <Fragment>
+        <h1 key="heading">No Events Found</h1>,
+        <p key="description">
+          You can pick a different category of events using the{' '}
+          <FilterIcon background="#F5F5F5" /> filter on the top.
+        </p>
+      </Fragment>
+    )}
 
     <style jsx>{styles}</style>
   </div>
