abe10928950263be602b56cd636add31cb716d30
Leo Lamprecht <mindrun@icloud.com>
2018-02-22 14:33:25 -0800

    More tests

--- test/index.js
+++ test/index.js
@@ -24,10 +24,28 @@ test('make sure we have 4 windows', async t => {
   t.is(await t.context.client.getWindowCount(), 4)
 })
 
-test('switch to tutorial window', async t => {
-  t.true(await changeWindow(t.context, 'tutorial'))
+test('enter gibberish into the login field', async t => {
+  const value = Math.random()
+    .toString(36)
+    .split('.')[1]
+  const selector = 'aside.login input'
+
+  await changeWindow(t.context, 'tutorial')
+  await t.context.client.setValue(selector, value)
+
+  t.is(await t.context.client.getValue(selector), value)
+})
+
+test('submit gibberish in the login field', async t => {
+  await t.context.client.keys('Enter')
+
+  const selector = 'aside.login'
+  const classes = await t.context.client.getAttribute(selector, 'class')
+
+  t.true(classes.split(' ').includes('error'))
 })
 
 test.after.always(async t => {
+  await new Promise(resolve => setTimeout(resolve, 5000))
   await t.context.stop()
 })
