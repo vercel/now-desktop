131385ef74f02d1e84f89e574a0db94cc83b9051
Pranay Prakash <pranay.gp@gmail.com>
2018-06-04 17:21:03 -0700

    Implements 'Tips'

---
+++ renderer/vectors/bulb.js
@@ -0,0 +1,19 @@
+// Packages
+import React from 'react'
+
+const Bulb = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="14"
+    viewBox="0 0 48 56"
+  >
+    <path
+      fill="#067DF7"
+      stroke="#067DF7"
+      d="M36 45v11H12V45A24 24 0 0 1 24 0a24 24 0 0 1 12 45zm-20 7h16v-4H16v4zm8-48a20 20 0 0 0-8 38v2h16v-2a20 20 0 0 0-8-38z"
+    />
+  </svg>
+)
+
+export default Bulb
