8d5e1914eaa4b7b62e659d73502ee1a6235aba41
Igor Klopov <igor@klopov.com>
2018-06-13 15:03:20 +0300

    add 'team-delete' event

---
+++ renderer/components/feed/messages/team-delete.js
@@ -0,0 +1,15 @@
+// Components
+import Message from './message'
+
+export default class TeamDelete extends Message {
+  render() {
+    const { event } = this.props
+
+    return (
+      <p>
+        {this.getDisplayName()}
+        deleted the team <b>{event.payload.slug}</b>
+      </p>
+    )
+  }
+}
--- renderer/components/feed/messages/team-member-role-update.js
+++ renderer/components/feed/messages/team-member-role-update.js
@@ -1,7 +1,7 @@
 // Components
 import Message from './message'
 
-export default class TeamMemberRollUpdate extends Message {
+export default class TeamMemberRoleUpdate extends Message {
   render() {
     const { event } = this.props
 
@@ -9,8 +9,8 @@ export default class TeamMemberRollUpdate extends Message {
       <p>
         {this.getDisplayName()}
         updated <b>{event.payload.updatedUser.username}</b>
-        {`'`}s roll from <b>{event.payload.previousRoll}</b>
-        to <b>{event.payload.roll}</b>
+        {`'`}s role from <b>{event.payload.previousRole}</b>
+        to <b>{event.payload.role}</b>
       </p>
     )
   }
