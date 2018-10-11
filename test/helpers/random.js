b5b176c2488cb85dfde1709d05a161dc406b2f69
Leo Lamprecht <mindrun@icloud.com>
2018-10-11 16:19:51 +0200

    Fix team switcher hover state (#541)
    * Fix team switcher hover state (#535)
    
    * Fix team switcher hover state
    
    * Fixed security issue
    
    * Downgraded package
    
    * Removed disfunctional tests
    
    * Removed from CI config

--- test/helpers/random.js
+++
@@ -1,9 +0,0 @@
-// Native
-const { createHash } = require('crypto')
-
-module.exports = () => {
-  const pre = Math.random().toString()
-  const generator = createHash('sha1').update(pre)
-
-  return generator.digest('hex')
-}
