ce36f71c1552fba0018b75cc8c8a28b5d7864b5e
Joe Cohen <joecohens@users.noreply.github.com>
2018-10-19 14:00:04 -0500

    Fix window position on multiple retina displays (#546)
    * fix window position on multiple retina screens
    
    * remove unnecessary check of boundaries

--- main/utils/frames/position.js
+++ main/utils/frames/position.js
@@ -3,18 +3,19 @@ const { platform } = require('os')
 
 // Packages
 const isEqual = require('react-fast-compare')
+const Positioner = require('electron-positioner')
 
 let trayBoundsCache = null
 let displayAreaCache = null
 
 module.exports = (tray, window) => {
-  // This module needs to be loaded after the app is ready
-  // I don't know why, but that's required by electron
   const { screen } = require('electron')
-  const display = screen.getDisplayNearestPoint(screen.getCursorScreenPoint())
+  const screenPoint = screen.getCursorScreenPoint()
+  const display = screen.getDisplayNearestPoint(screenPoint)
   const displayArea = display.workArea
 
   const trayBounds = tray.getBounds()
+
   const notMacOS = platform() !== 'darwin'
 
   if (trayBoundsCache && displayAreaCache) {
@@ -27,39 +28,16 @@ module.exports = (tray, window) => {
     }
   }
 
-  // Cache the tray and display positions
   trayBoundsCache = trayBounds
   displayAreaCache = displayArea
 
-  const windowSize = window.getSize()
+  const positioner = new Positioner(window)
 
-  let horizontalPosition
-  let verticalPosition
+  const windowPosition = notMacOS ? 'trayBottomCenter' : 'trayCenter'
 
-  if (notMacOS) {
-    horizontalPosition = displayArea.x + displayArea.width - windowSize[0]
-    verticalPosition = displayArea.y + displayArea.height - windowSize[1]
-  } else {
-    const trayCenter = trayBounds.x + trayBounds.width / 2
-    horizontalPosition = trayCenter - windowSize[0] / 2
+  const { x, y } = positioner.calculate(windowPosition, trayBoundsCache)
 
-    // The macOS implementation of Electron.Tray ceils trayBounds.y to zero
-    // making it unreliable for vertically positioning the window.
-    // Use the display's work area instead.
-    verticalPosition = displayArea.y + 7
+  const vertical = notMacOS ? y : y + 7
 
-    if (screen.getPrimaryDisplay().workArea.height === 0) {
-      verticalPosition += 22
-    }
-    const left = horizontalPosition + windowSize[0]
-    const maxLeft = displayArea.width - 18
-
-    // Check if window would be outside screen
-    // If yes, make sure it isn't
-    if (left > maxLeft) {
-      horizontalPosition -= left - maxLeft
-    }
-  }
-
-  window.setPosition(horizontalPosition, verticalPosition)
+  window.setPosition(x, vertical)
 }
