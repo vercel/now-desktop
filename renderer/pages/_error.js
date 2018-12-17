2a5b389113b0cae7faf331b510e1753e05b61f91
Leo Lamprecht <mindrun@icloud.com>
2018-12-17 14:33:01 +0100

    Fixed error reporting (#580)
    * Fixed error reporting
    
    * Fixed in renderer too

--- renderer/pages/_error.js
+++ renderer/pages/_error.js
@@ -8,7 +8,7 @@ import pkg from '../../package'
 if (!isDev) {
   Sentry.init({
     dsn: 'https://d07ceda63dd8414e9c403388cfbd18fe@sentry.io/1323140',
-    environment: pkg.includes('canary') ? 'canary' : 'stable',
+    environment: pkg.version.includes('canary') ? 'canary' : 'stable',
     release: `now-desktop@${pkg.version}`
   })
 }
