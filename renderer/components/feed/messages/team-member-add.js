d7b0173ae862023a6224f9ed6cf3b5155d01d9b8
Andy <AndyBitz@users.noreply.github.com>
2019-02-04 22:13:26 +0100

    Handle missing payload (#598)

--- renderer/components/feed/messages/team-member-add.js
+++ renderer/components/feed/messages/team-member-add.js
@@ -5,13 +5,24 @@ export default class TeamMemberAdd extends Message {
   render() {
     const { event } = this.props
     const invitedUser = event.payload.invitedUser
-    const username = invitedUser.username || invitedUser.email
+
+    const email = invitedUser && invitedUser.email
+    const username = invitedUser && invitedUser.username
+    const displayName = email || username
+
+    if (displayName) {
+      return (
+        <p>
+          {this.getDisplayName()}
+          invited user{email ? ' with email address' : ''} <b>{displayName}</b>
+        </p>
+      )
+    }
 
     return (
       <p>
         {this.getDisplayName()}
-        invited user{invitedUser.username ? '' : ' with email address'}{' '}
-        <b>{username}</b>
+        invited a new user
       </p>
     )
   }
