9877705a5291aa00f4326b7de028e2aaf1ad0daa
Andy <AndyBitz@users.noreply.github.com>
2019-03-06 22:59:57 +0100

    Add the `alias-system` event (#605)
    * Added the `alias-system` event
    
    * Fix the missing space
    
    * Update renderer/components/feed/messages/alias-system.js
    
    Co-Authored-By: AndyBitz <AndyBitz@users.noreply.github.com>

---
+++ renderer/components/feed/messages/alias-system.js
@@ -0,0 +1,15 @@
+// Components
+import Message from './message'
+
+export default class AliasSystem extends Message {
+  render() {
+    const { event } = this.props
+
+    return (
+      <p>
+        The staging alias <b>{event.payload.alias}</b> was assigned to{' '}
+        <b>{event.payload.deploymentUrl}</b>
+      </p>
+    )
+  }
+}
