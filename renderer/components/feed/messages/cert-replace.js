d71e85a5f10d71c44bfb8979cf5e969383dda56f
Andy Bitz <artzbitz@gmail.com>
2019-03-23 13:01:20 +0100

    Fix cert events

--- renderer/components/feed/messages/cert-replace.js
+++ renderer/components/feed/messages/cert-replace.js
@@ -1,14 +1,48 @@
 // Components
+import { Fragment } from 'react'
 import Message from './message'
 
 export default class CertReplace extends Message {
   render() {
     const { event } = this.props
+    const { cn, cns } = event.payload
+
+    let domains = cns || cn
+    domains = Array.isArray(domains) ? domains : [domains]
+    domains = domains.map(domain => <b key={domain}>{domain}</b>)
 
     return (
       <p>
         {this.getDisplayName()}
-        replaced a certificate for <b>{event.payload.cn}</b>
+        replaced a certificate for{' '}
+        {domains.map((domain, index) => {
+          const hasNext = domains[index + 1]
+          const isLast = domain === domains[domains.length - 1]
+          const isPreLast = domain === domains[domains.length - 2]
+
+          if (isLast && domains.length > 1) {
+            return (
+              <Fragment key={domain}>
+                {' '}
+                and <b>{domain}</b>
+              </Fragment>
+            )
+          }
+
+          if (hasNext && !isPreLast) {
+            return (
+              <Fragment key={domain}>
+                <b>{domain}</b>,{' '}
+              </Fragment>
+            )
+          }
+
+          return (
+            <Fragment key={domain}>
+              <b>{domain}</b>
+            </Fragment>
+          )
+        })}
       </p>
     )
   }
