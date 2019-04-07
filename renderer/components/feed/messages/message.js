08e04f5e3c865a04b99c2aa06d6d4c8f30f9e746
Leo Lamprecht <mindrun@icloud.com>
2019-04-07 16:46:31 +0200

    Correctly mention who authored an event

--- renderer/components/feed/messages/message.js
+++ renderer/components/feed/messages/message.js
@@ -1,24 +1,9 @@
-// Packages
 import { PureComponent } from 'react';
 import { object } from 'prop-types';
 
 class Message extends PureComponent {
   getDisplayName() {
-    const { event, user } = this.props;
-
-    let isCurrentUser = false;
-
-    if (event.userId && event.userId === user.uid) {
-      isCurrentUser = true;
-    }
-
-    if (event.user && event.user.uid && event.user.uid === user.uid) {
-      isCurrentUser = true;
-    }
-
-    if (isCurrentUser) {
-      return [<b key="you">You</b>, ' '];
-    }
+    const { event } = this.props;
 
     if (event.user.username) {
       return [<b key="username">{event.user.username}</b>, ' '];
