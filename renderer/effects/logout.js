9fc920bbceb54068c8dffa6edccdcb44a430ba31
Max Rovensky <hey@fivepointseven.com>
2019-05-10 00:01:00 +0800

    Clean up logout effect

--- renderer/effects/logout.js
+++ renderer/effects/logout.js
@@ -2,4 +2,8 @@ import ipc from '../utils/ipc';
 
 export default (_, handleLogout) => {
   ipc.onLoggedOut(handleLogout);
+
+  return () => {
+    ipc.clearLoggedOutListener(handleLogout);
+  };
 };
